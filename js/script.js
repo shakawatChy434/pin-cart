cc7e368e19f3b3a18a7b9a978151dda2056c6d1c
sakawatchy434 <hossainshakawat434@gmail.com>
2021-08-19 12:57:38 -0700

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-console.log('Im Your new patner');
+/* 
+function getPin() {
+    // document.getElementById('generate-pin-no').addEventListener('click', function () {
+    // const generateInput = document.getElementById('generate-input');
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    } else {
+        console.log('get 3 digit pin & calling again the function', pin);
+        return getPin();
+    }
+    // });
+
+};
+function generatePin() {
+    console.log(pin);
+}; */
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    } else {
+        // console.log('got this pin')
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const pin = getPin();
+    const displayPin = document.getElementById('display-pin').value = pin;
+    // console.log(pin);
+}
